536cef031b9b62848c63fd0e8aca5df4861c9acc
Andrew <8265381-andtech@users.noreply.gitlab.com>
2022-05-17 21:03:01 -0700

    init

---
+++ Augment/Assets/com.andtech.augment/Editor/Scripts/MenuActions.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace Andtech.Augment
+{
+
+	public class MenuActions : ScriptableObject
+	{
+		private static EditorWindow GameWindow => EditorWindow.GetWindow(typeof(EditorWindow).Assembly.GetType("UnityEditor.GameView"));
+		private static bool autoMaximize;
+		private static bool wasMaximized;
+
+		static MenuActions()
+		{
+			EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
+		}
+
+		[MenuItem("Edit/Andtech/Run _F4")]
+		public static void Play()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Play");
+		}
+
+		[MenuItem("Edit/Andtech/Pause _F5")]
+		public static void Pause()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Pause");
+		}
+
+		[MenuItem("Edit/Andtech/Step _F6")]
+		public static void Step()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Step");
+		}
+
+		[MenuItem("Edit/Andtech/Run Maximized #_F5")]
+		public static void PlayMaximized()
+		{
+			if (!EditorApplication.isPlaying)
+			{
+				autoMaximize = true;
+				wasMaximized = GameWindow.maximized;
+
+				GameWindow.maximized = true;
+			}
+			EditorApplication.ExecuteMenuItem("Edit/Play");
+		}
+
+		[MenuItem("Edit/Andtech/Maximize (Force) %#UP")]
+		public static void MaximizeForce()
+		{
+			GameWindow.maximized = true;
+		}
+
+		[MenuItem("Edit/Andtech/Unmaximize (Force) %#DOWN")]
+		public static void UnmaximizeForce()
+		{
+			GameWindow.maximized = false;
+		}
+
+		[MenuItem("File/Show Project in Explorer %#E", priority = 199)]
+		public static void ShowInExplorer()
+		{
+			string path = Directory.GetParent(Application.dataPath).FullName;
+			EditorUtility.RevealInFinder(path);
+		}
+
+		[MenuItem("Edit/Launch...", priority = 185)]
+		public static void Launch()
+		{
+			var buildDir = Path.Combine(Environment.CurrentDirectory, "builds");
+			string extension;
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				extension = ".exe";
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				extension = ".app";
+			}
+			else
+			{
+				throw new InvalidOperationException("The current platform is not supported.");
+			}
+
+			var binaries = Directory.EnumerateFiles(buildDir, $"*{extension}", SearchOption.TopDirectoryOnly);
+			if (binaries.Any())
+			{
+				var binary = binaries.First();
+				Process.Start(binary);
+			}
+			else
+			{
+				UnityEngine.Debug.LogError($"No binaries found in {buildDir}");
+			}
+		}
+
+		[MenuItem("Edit/Andtech/Clear Console %#L")]
+		public static void ClearConsole()
+		{
+			Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+			Type type = assembly.GetType("UnityEditor.LogEntries");
+			MethodInfo method = type.GetMethod("Clear");
+			method.Invoke(new object(), null);
+		}
+
+		private static void EditorApplication_playModeStateChanged(PlayModeStateChange obj)
+		{
+			if (obj == PlayModeStateChange.EnteredEditMode)
+			{
+				if (autoMaximize && !wasMaximized)
+				{
+					GameWindow.maximized = false;
+				}
+
+				autoMaximize = false;
+			}
+		}
+	}
+}
